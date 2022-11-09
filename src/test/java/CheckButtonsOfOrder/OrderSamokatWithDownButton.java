a509cca2b40e46dfa426c4ee2986df0696cff3ed
Anastasia Simolova <simolovaai@gmail.com>
2022-11-09 15:12:01 +0300

    Tests with new class

--- src/test/java/CheckButtonsOfOrder/OrderSamokatWithDownButton.java
+++ src/test/java/CheckButtonsOfOrder/OrderSamokatWithDownButton.java
@@ -1,32 +1,63 @@
 package CheckButtonsOfOrder;
 
-import main.BrowserChoice;
-import org.junit.Rule;
 import org.junit.Test;
-import PageObject.FirstPageOrder;
-import PageObject.MainPageSamokat;
-import PageObject.SecondPageOrder;
+import org.junit.After;
+import pageobject.FirstPageOrder;
+import pageobject.MainPageSamokat;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+import java.io.File;
 
 public class OrderSamokatWithDownButton {
 
-    @Rule
-    public BrowserChoice browserChoice = new BrowserChoice();
+    private WebDriver driver;
+
+    private final String textExpected = "Заказ оформлен";
 
     @Test
-    public void OrderWithTopButton() {
+    // тест заказа через нижнию кнопку в FireFox (Успешный)
+    public void orderWithDownButtonInFirefox() {
+        // драйвер для браузера ff
+        GeckoDriverService gecko = new GeckoDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\WebDriver\\bin\\geckodriver.exe"))
+                .usingFirefoxBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")))
+                .build();
+        driver = new FirefoxDriver(gecko);
 
-        MainPageSamokat objMainPageForDownButton = new MainPageSamokat(browserChoice.driver);
+        MainPageSamokat objMainPage = new MainPageSamokat(driver);
+        objMainPage.open();
+        objMainPage.clickDownButton();
 
-        objMainPageForDownButton.open();
-
-        objMainPageForDownButton.clickDownButton();
-
-        FirstPageOrder objFirstPageOrder = new FirstPageOrder(browserChoice.driver);
-
-        objFirstPageOrder.enterInFirstForm("Иван","Иванкович", "Нижегородская, 1", "89101234567");
-
-        SecondPageOrder objSecondPageOrder = new SecondPageOrder(browserChoice.driver);
-
-        objSecondPageOrder.enterInSecondForm("03.11.2022","Жду свой самокат!");
+        FirstPageOrder objOrderPage = new FirstPageOrder(driver);
+        objOrderPage.fillInFirstForm("Иван", "Иванкович", "Нижегородская, 1", "Черкизовская", "89101234567");
+        objOrderPage.fillInSecondForm("03.11.2022", "Жду свой самокат!");
+        objOrderPage.orderCreationSuccessful();
     }
-}
+
+    @Test
+    // тест заказа через нижнию кнопку в Google Chrome (Баг!)
+    public void orderWithDownButtonInChrome() {
+        // драйвер для браузера Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments();
+        driver = new ChromeDriver(options);
+
+        MainPageSamokat objMainPage = new MainPageSamokat(driver);
+        objMainPage.open();
+        objMainPage.clickDownButton();
+
+        FirstPageOrder objOrderPage = new FirstPageOrder(driver);
+        objOrderPage.fillInFirstForm("Иван", "Иванкович", "Нижегородская, 1", "Черкизовская", "89101234567");
+        objOrderPage.fillInSecondForm("03.11.2022", "Жду свой самокат!");
+        objOrderPage.orderCreationSuccessful();
+    }
+    @After
+    public void teardown() {
+        // close br
+        driver.quit();
+    }
--- src/test/java/CheckButtonsOfOrder/OrderSamokatWithTopButton.java
+++ src/test/java/CheckButtonsOfOrder/OrderSamokatWithTopButton.java
@@ -1,31 +1,59 @@
 package CheckButtonsOfOrder;
 
-import main.BrowserChoice;
-import org.junit.Rule;
+import org.junit.After;
 import org.junit.Test;
-import PageObject.FirstPageOrder;
-import PageObject.MainPageSamokat;
-import PageObject.SecondPageOrder;
+import pageobject.FirstPageOrder;
+import pageobject.MainPageSamokat;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+import java.io.File;
 
 public class OrderSamokatWithTopButton {
+    private WebDriver driver;
+    @Test
+    // тест заказа через кнопку сверху в FireFox (Успешный)
+    public void orderWithTopButtonInFireFox() {
+        // драйвер для браузера ff
+        GeckoDriverService gecko = new GeckoDriverService.Builder()
+                .usingDriverExecutable(new File("C:\\WebDriver\\bin\\geckodriver.exe"))
+                .usingFirefoxBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")))
+                .build();
+        driver = new FirefoxDriver(gecko);
 
-    @Rule
-    public BrowserChoice browserChoice = new BrowserChoice();
+        MainPageSamokat objMainPage = new MainPageSamokat(driver);
+        objMainPage.open();
+        objMainPage.clickTopButton();
+
+        FirstPageOrder objPageOrder = new FirstPageOrder(driver);
+        objPageOrder.fillInFirstForm("Петр","Петров", "Родионова, 23", "Измайлово", "89990002233");
+        objPageOrder.fillInSecondForm("04.11.2022","Хочу кататься");
+        objPageOrder.orderCreationSuccessful();}
 
     @Test
-    public void OrderWithTopButton() {
+    // тест заказа через кнопку сверху в Google Chrome (Баг!)
+    public void orderWithTopButtonInChrome() {
+        // драйвер для браузера Chrome
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments();
+        driver = new ChromeDriver(options);
 
-        MainPageSamokat objMainPageForTopButton = new MainPageSamokat(browserChoice.driver);
+        MainPageSamokat objMainPage = new MainPageSamokat(driver);
+        objMainPage.open();
+        objMainPage.clickTopButton();
 
-        objMainPageForTopButton.open();
+        FirstPageOrder objPageOrder = new FirstPageOrder(driver);
+        objPageOrder.fillInFirstForm("Петр","Петров", "Родионова, 23", "Измайлово", "89990002233");
+        objPageOrder.fillInSecondForm("04.11.2022","Хочу кататься");
+        objPageOrder.orderCreationSuccessful();}
 
-        objMainPageForTopButton.clickTopButton();
-
-        FirstPageOrder objFirstOrderPage = new FirstPageOrder(browserChoice.driver);
-
-        objFirstOrderPage.enterInFirstForm("Петр","Петров", "Родионова, 23", "89990002233");
-
-        SecondPageOrder objSecondPageOrder = new SecondPageOrder(browserChoice.driver);
-
-        objSecondPageOrder.enterInSecondForm("04.11.2022","Хочу кататься");    }
+    @After
+    public void teardown() {
+        // close br
+        driver.quit();
+    }
 }
